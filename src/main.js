2dded2412c2693a5369d9cd3a957c44e9e3e942c
Joey Thomas <joeypunkrock11@gmail.com>
2018-12-19 14:56:00 +0000

    added jQuery plugin skeleton

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-//whack ya code in here
+import './main.scss';
+
+(function($) {
+
+  'use strict';
+
+  const MyPlugin = function() {
+
+    // Public method - can be called from client code
+    this.publicMethod = function() {
+        console.log('public method called!');
+    };
+
+    // Private method - can only be called from within this object
+    var privateMethod = function() {
+        console.log('private method called!');
+    };
+
+  };
+
+  //return the object for global use
+  $.myPlugin = function() {
+    return new MyPlugin();
+  }
+
+})(jQuery);
+
+//export for package
+export default $.myPlugin();
